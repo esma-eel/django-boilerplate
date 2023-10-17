e31e3c48bef72b1bc6e927b082a0c4ed3b76300c
Esmaeel Komijani X5 <esmaeelkomijani@gmail.com>
2023-10-17 17:15:36 +0330

    profile models changed

--- boilerplate/profiles/models.py
+++ boilerplate/profiles/models.py
@@ -5,23 +5,6 @@ import uuid
 from .utils.upload_path import profile_upload
 
 
-class Profile(ModelMixin):
-    uuid = models.UUIDField(
-        _("UUID"), unique=True, default=uuid.uuid4, editable=False
-    )
-    name = models.CharField(_("Name"), max_length=32, blank=True, null=True)
-    avatar = models.ImageField(
-        _("Avatar"), blank=True, null=True, upload_to=profile_upload
-    )
-
-    user = models.OneToOneField(
-        "users.User", on_delete=models.PROTECT, verbose_name=_("User")
-    )
-
-    def __str__(self):
-        return self.name
-
-
 class ProfileEmail(ModelMixin):
     email = models.EmailField(_("Email"), unique=True)
     is_primary = models.BooleanField(default=False)
@@ -60,3 +43,44 @@ class ProfileAddress(ModelMixin):
 
     def __str__(self):
         return f"{self.profile} Address"
+
+
+class Profile(ModelMixin):
+    uuid = models.UUIDField(
+        _("UUID"), unique=True, default=uuid.uuid4, editable=False
+    )
+    name = models.CharField(_("Name"), max_length=32, blank=True, null=True)
+    avatar = models.ImageField(
+        _("Avatar"), blank=True, null=True, upload_to=profile_upload
+    )
+
+    user = models.OneToOneField(
+        "users.User", on_delete=models.PROTECT, verbose_name=_("User")
+    )
+
+    def __str__(self):
+        return self.name
+
+    def get_phone_numbers(self):
+        return self.phone_number_set.all()
+
+    def get_primary_phone_number(self):
+        qs_phone_numbers = self.get_phone_numbers()
+        qs_primary_phone_number = qs_phone_numbers.filter(is_primary=True)
+
+        if qs_primary_phone_number.exists():
+            return qs_primary_phone_number.last().number
+
+        return None
+
+    def get_emails(self):
+        return self.email_set.all()
+
+    def get_primary_email(self):
+        qs_emails = self.get_emails()
+        qs_primary_email = qs_emails.filter(is_primary=True)
+
+        if qs_primary_email.exists():
+            return qs_primary_email.last().email
+
+        return None
