78ea28d1e30ec51acb6991522d78c7a94c6f98ca
Esmaeel Komijani X5 <esmaeelkomijani@gmail.com>
2024-01-12 22:49:47 +0330

    removed extra import

--- boilerplate/common/tests/test_mixins.py
+++ boilerplate/common/tests/test_mixins.py
@@ -3,7 +3,6 @@
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from boilerplate.profiles.models import (
-    Profile,
     ProfilePhoneNumber,
     ProfileEmail,
     ProfileAddress,
